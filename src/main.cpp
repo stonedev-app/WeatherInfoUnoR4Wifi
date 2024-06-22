07362b7bfc48c99a2e8d72bdc106aa38e80851f3
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2024-06-22 09:10:01 +0900

    feat: Display the content of the JSON received from the weather forecast API on the LED matrix

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 #include <WiFiSSLClient.h>
+#include <Arduino_LED_Matrix.h>
 
 #include <Arduino_JSON.h>
+
+#include "misakiUTF16.h"
 
 #include "arduino_secrets.h"
 #include "wifi_connection_manage.h"
@@ -18,6 +21,94 @@ WifiConnectionManage wifiConManage;
 // Client management class
 WifiClientManage client;
 
+ArduinoLEDMatrix matrix;          // LEDマトリックスドライバ
+unsigned long frame[] = {0,0,0};  // 12x8ドット フレームバッファ
+
+// フレームバッファの指定座標にドットをセット
+void setAt(uint8_t x, uint8_t y, uint8_t color) {
+  if (x>=12 || y >=8) {
+    return;
+  }  
+  
+  uint8_t bitno = y*12+x;      // 書込み位置を96ビットの連番に変換
+  uint8_t index = bitno / 32;  // 連番からlongバイト配列位置を取得
+  uint8_t pos   = bitno % 32;  // 配列内ビット位置を取得
+  
+  // 指定位置のビットにデータをセットする
+  if (color) {
+    frame[index]|=0b10000000000000000000000000000000>>pos;      // ドットのセット
+  } else {
+    frame[index]&=~(0b10000000000000000000000000000000>>pos);   // ドットのリセット
+  }
+}
+
+// フレームバッファの1ドット左スクロール
+void scroll() {
+  uint8_t mlb[3]; // キャリーオーバービットの退避用
+  
+  // longデータの左ビットシフト
+  for (uint8_t i=0; i<3; i++) {
+    mlb[i] = frame[i] & 0b10000000000000000000000000000000 ?1:0;  // MSBのキャリーオーバービットの退避
+    frame[i]=frame[i]<<1; // 左ビットシフト
+  }
+
+  // 0～7行の右端ビットクリアのためのマスク処理
+  frame[0] &= 0b11111111111011111111111011111111;
+  frame[1] &= 0b11101111111111101111111111101111;
+  frame[2] &= 0b11111110111111111110111111111110;
+
+  // キャリーオーバービットを元に戻す
+  frame[0] = mlb[1] ? frame[0]|1:frame[0]&~1;
+  frame[1] = mlb[2] ? frame[1]|1:frame[1]&~1;
+}
+
+// フレームバッファのクリア
+void clear() {
+   for (uint8_t i=0; i<3; i++) {
+    frame[i] = 0;
+   }
+}
+
+// 1文字左スクロール挿入
+void scrollIn(uint8_t* font, uint8_t fw, uint8_t ypos, int wt) {
+    int8_t wx,wy;
+
+    // 1文字分 横幅fwドットフォントの挿入(縦位置オフセットyposを考慮)
+    for (wx = 0; wx < fw; wx++) {
+        scroll(); // 左に1ドットスクロール
+        for (wy = 0; wy < 8; wy++) {
+          setAt(11, wy+ypos, font[wy] & 0x80>>wx ? 1:0);
+        }
+        matrix.loadFrame(frame);
+        delay(wt);
+    }
+}
+
+// デモ4 漢字フォント利用
+void demo(const char* dispString) {
+  uint8_t font[8];
+  uint8_t width;     // フォント幅
+
+  // テスト用文字列
+  char msg_str[] = "あいうえお123abcABCｱｲｳｴｵ、今日は3月18日です。かんたんなかんじの表示ができます。";
+
+  size_t len = strlen(dispString);
+  char *temp = new char[len]();
+  strcpy(temp, dispString);
+
+  char *str = temp;
+
+  while(*str) {
+    uint16_t ucode;  // utf16文字コード
+    str += charUFT8toUTF16(&ucode, str);      // utf8 1文字分をutf16に変換
+    width = isZenkaku(ucode) ? 8: 4;          // フォント幅の取得
+    getFontDataByUTF16(font, ucode);          // フォントデータの取得
+    scrollIn(font, width, 1, 80);             // 1文字分のスクロール表示
+  }
+
+  delete[] temp;
+}
+
 void setup()
 {
   // Initialize serial and wait for port to open:
@@ -27,6 +118,8 @@ void setup()
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
+  matrix.begin();
+
   // WiFi connection
   wifiConManage.init(SECRET_SSID, SECRET_PASS);
 
@@ -35,6 +128,8 @@ void setup()
 
   // GET request
   client.get(JMA_ROOT_CA, JMA_HOST, WFS_TOKYO, response);
+
+  demo((const char *)response["text"]);
 
 }
 
